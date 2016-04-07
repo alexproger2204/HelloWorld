7ab4694e3546dc3375d0df18877a5335359c9de3
my name <my@email>
2016-04-07 13:22:25 +0300

    Entry point add

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 /**
  * Created by IIS on 07.04.2016.
  */
-public class Main {
-    
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("HELLO SASHAPROHER2204");
+    }
+
 }
